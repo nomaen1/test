bc59fb4988d043df5cb17006a33dfec79b1368c0
Janysh <tynybekovjanyshbek@gmail.com>
2023-03-14 20:34:00 +0600

    test

---
+++ class1.py
@@ -0,0 +1,9 @@
+class Hello:
+    def __init__(self, str):
+        self.str = str
+
+from art import tprint
+
+class A(Hello):
+    def method():
+        tprint("PYTHON")
---
+++ task.py
@@ -0,0 +1,5 @@
+# from art import tprint
+# tprint("PYTHON")
+
+from class1 import A
+A.method()
